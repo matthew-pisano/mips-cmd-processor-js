077a9319cd14c10c8c6efbb8bf734ec1ac1573f0
matthew-pisano <matthewpisano14@gmail.com>
2025-09-01 13:42:46 -0400

    Add Http Server and Stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: "stylelint-config-standard",
+    rules: {
+        "selector-class-pattern": "^[a-z][a-zA-Z0-9-]+$",
+        "selector-id-pattern": "^[a-z][a-zA-Z0-9-]+$"
+    }
+};
